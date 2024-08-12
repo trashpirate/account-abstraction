c2f335ed2ecc463390d8fc88258d47d6d1523524
trashpirate <nadinaoates@gmail.com>
2024-08-12 12:24:22 -0500

    add signature validation

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity 0.8.24;
 
 /**
  * IMPORTS
  */
 import {IAccount} from "account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "account-abstraction/contracts/core/Helpers.sol";
 /**
  * INTERFACES
  */
@@ -19,7 +23,7 @@ import {PackedUserOperation} from "account-abstraction/contracts/interfaces/Pack
  * @author Nadina Oates
  * @notice ERC-4337
  */
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
     /**
      * TYPES
      */
@@ -43,7 +47,7 @@ contract MinimalAccount is IAccount {
     /**
      * CONSTRUCTOR
      */
-    constructor(address owner) {}
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
     /**
      * RECEIVE/FALLBACK
@@ -55,7 +59,12 @@ contract MinimalAccount is IAccount {
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         returns (uint256 validationData)
-    {}
+    {
+        // signature is valid if it is contract owner (0: valid, 1: not valid)
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce()
+        _payPrefund(missingAccountFunds);
+    }
 
     /**
      * PUBLIC
@@ -65,6 +74,26 @@ contract MinimalAccount is IAccount {
      * INTERNAL
      */
 
+    // EIP-191 version of the signed hash
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
     /**
      * PRIVATE
      */
