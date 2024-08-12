75af4a47d4f60dd8efb567b799adc41d5b1adf8f
Nadina Oates <36479450+trashpirate@users.noreply.github.com>
2024-08-11 22:52:13 -0500

    Initial commit

---
+++ src/Contract.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+/**
+ * IMPORTS
+ */
+
+/**
+ * INTERFACES
+ */
+
+/**
+ * LIBRARIES
+ */
+
+/**
+ * @title
+ * @author Nadina Oates
+ * @notice
+ */
+contract Contract {
+    /**
+     * TYPES
+     */
+
+    /**
+     * STATE VARIABLES
+     */
+
+    /**
+     * EVENTS
+     */
+
+    /**
+     * ERRORS
+     */
+
+    /**
+     * MODIFIERS
+     */
+
+    /**
+     * CONSTRUCTOR
+     */
+    constructor(address owner) {}
+
+    /**
+     * RECEIVE/FALLBACK
+     */
+
+    /**
+     * EXTERNAL
+     */
+
+    /**
+     * PUBLIC
+     */
+
+    /**
+     * INTERNAL
+     */
+
+    /**
+     * PRIVATE
+     */
+}
